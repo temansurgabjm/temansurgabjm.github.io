ad3a98128a9f849640253cf668bb03ba19989939
Muhammad Firdaus <mfirdaus1453@gmail.com>
2022-03-19 22:51:23 +0800

    fix position

--- index.js
+++ index.js
@@ -4,20 +4,22 @@ var image_array = []
 for (var i = 0; i <= totalImag; i++) {
     image_array[i] = i + ".jpg";
 }
-console.log(image_array.length);
-console.log(image_array);
 
 function get_random_image() {
     // Get a random index
-    random_index = Math.floor(Math.random() * image_array.length);
+    random_index = get_random_index(image_array)
 
+    // Get an image at the random_index
     if (image_array[random_index] != "0.jpg") {
-        // Get an image at the random_index
         selected_image = image_array[random_index]
-        console.log(image_array[random_index]);
+    } else {
+        selected_image = image_array[get_random_index(image_array)]
     }
-
 
     // Display the image
     document.getElementById('image_shower').src = `./images/${selected_image}`
+}
+
+function get_random_index(image_array) {
+    return Math.floor(Math.random() * image_array.length);
 }
