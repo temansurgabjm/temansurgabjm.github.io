8cabea604e644a88891aec36efb58ae6f7382ab6
Muhammad Firdaus <mfirdaus1453@gmail.com>
2022-03-20 00:50:42 +0800

    add macam2

--- index.js
+++ index.js
@@ -1,25 +1,50 @@
 // Get all the images
 let totalImag = 19
 var image_array = []
-var selected_image = ""
+let interval;
+let isFirstTime = true;
+let urlDaftar = "https://api.whatsapp.com/send/?phone=6289647476858&text=%2AAssalamualaikum.+Wr.+Wb.%2A+Kak%2C+Saya+mau+ikut+Acara+Ramadhan%0A%0A%2ANama%3A%0A%2AUsia%3A%0A%2ASekolah%3A%0A%2AAsal+kota%3A%0A&app_absent=0"
+let intervalSpeed = 10;
+let delayRandom = 5000;
 
 for (var i = 0; i <= totalImag; i++) {
     image_array[i] = i + ".jpg";
 }
 
 function get_random_image() {
-    // Get a random index
-    random_index = get_random_index(image_array)
+    if (isFirstTime) {
+        const button = document.getElementById('btn')
+        button.disabled = true;
+        button.style.visibility = 'hidden'
+        button.innerText = "Lagi di acak";
 
-    // Get an image at the random_index
-    if (image_array[random_index] != "0.jpg") {
-        selected_image = image_array[random_index]
+        startRandomImage()
+
+        setTimeout(function() {
+            clearInterval(interval)
+            interval = null;
+            button.disabled = false;
+            button.innerText = "Daftar";
+            button.style.visibility = 'visible'
+            isFirstTime = false;
+        }, delayRandom);
+    } else {
+        window.location = urlDaftar;
     }
-
-    // Display the image
-    document.getElementById('image_shower').src = `./images/${selected_image}`
 }
 
 function get_random_index(image_array) {
     return Math.floor(Math.random() * image_array.length);
+}
+
+function startRandomImage() {
+    interval = setInterval(function() {
+        // Genarate image number
+        const imageNumber = Math.floor(Math.random() * image_array.length);
+        // Show image with the generated number
+        if (image_array[imageNumber] != "0.jpg") {
+            document.getElementById('image_shower').src = `./images/${image_array[imageNumber]}`
+            console.log(image_array[imageNumber]);
+        }
+    }, intervalSpeed);
 }
