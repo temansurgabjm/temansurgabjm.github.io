fa799259eb6637866350a686b07d40aa4d300876
Muhammad Firdaus <mfirdaus1453@gmail.com>
2022-03-19 21:55:24 +0800

    fix error image

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Get all the images
-let totalImag = 20
+let totalImag = 19
 var image_array = []
-for (var i = 1; i < totalImag; i++) {
+for (var i = 0; i <= totalImag; i++) {
     image_array[i] = i + ".jpg";
 }
 console.log(image_array.length);
@@ -11,8 +11,12 @@ function get_random_image() {
     // Get a random index
     random_index = Math.floor(Math.random() * image_array.length);
 
-    // Get an image at the random_index
-    selected_image = image_array[random_index]
+    if (image_array[random_index] != "0.jpg") {
+        // Get an image at the random_index
+        selected_image = image_array[random_index]
+        console.log(image_array[random_index]);
+    }
+
 
     // Display the image
     document.getElementById('image_shower').src = `./images/${selected_image}`
