b2929d5b2222e690804ae4cf29c0894dcd495af8
Muhammad Firdaus <mfirdaus1453@gmail.com>
2022-03-20 15:42:48 +0800

    revisi

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var image_array = []
 let interval;
 let isFirstTime = true;
 let urlDaftar = "https://api.whatsapp.com/send/?phone=6289647476858&text=%2AAssalamualaikum.+Wr.+Wb.%2A+Kak%2C+Saya+mau+ikut+Acara+Ramadhan%0A%0A%2ANama%3A%0A%2AUsia%3A%0A%2ASekolah%3A%0A%2AAsal+kota%3A%0A&app_absent=0"
-let intervalSpeed = 20;
+let intervalSpeed = 10;
 let delayRandom = 5000;
 
 for (var i = 0; i <= totalImage; i++) {
