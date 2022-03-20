da920b0c21f3d4efab149a574886f7cad65643df
Muhammad Firdaus <mfirdaus1453@gmail.com>
2022-03-20 09:41:08 +0800

    redesign with bootstrap

--- index.js
+++ index.js
@@ -6,41 +6,71 @@ let isFirstTime = true;
 let urlDaftar = "https://api.whatsapp.com/send/?phone=6289647476858&text=%2AAssalamualaikum.+Wr.+Wb.%2A+Kak%2C+Saya+mau+ikut+Acara+Ramadhan%0A%0A%2ANama%3A%0A%2AUsia%3A%0A%2ASekolah%3A%0A%2AAsal+kota%3A%0A&app_absent=0"
 let intervalSpeed = 20;
 let delayRandom = 5000;
+var imageNumber;
+
+// config
+var lastClick = 0;
+var delay = 20;
 
 for (var i = 0; i <= totalImag; i++) {
     image_array[i] = i + ".jpg";
 }
 
+document.getElementById('image_shower').onclick = function () {
+    console.log('You clicked');
+    get_random_image();
+}
+
+document.getElementById('btn').onclick = function () {
+    console.log('You clicked btn');
+    get_random_image();
+}
+
 function get_random_image() {
+    console.log("getRandomImage");
+    // preventMultipleClick()
+    const img = document.getElementById('image_shower')
+    const button = document.getElementById('btn')
+
     if (isFirstTime) {
-        const button = document.getElementById('btn')
-        button.disabled = true;
+        console.log("isFirstTime");
+        img.onclick = false;
         button.style.visibility = 'hidden'
         button.innerText = "Lagi di acak";
 
         startRandomImage()
 
-        setTimeout(function() {
+        setTimeout(function () {
+            const button = document.getElementById('btn')
+            console.log("TimeOut End");
             clearInterval(interval)
             interval = null;
-            button.disabled = false;
             button.innerText = "Daftar";
             button.style.visibility = 'visible'
             isFirstTime = false;
+            console.log(image_array[imageNumber]);
         }, delayRandom);
     } else {
+        console.log("is not first time");
         window.location = urlDaftar;
     }
 }
 
 function startRandomImage() {
-    interval = setInterval(function() {
+    console.log("Random Start");
+    interval = setInterval(function () {
         // Genarate image number
-        const imageNumber = Math.floor(Math.random() * image_array.length);
+        imageNumber = Math.floor(Math.random() * image_array.length);
         // Show image with the generated number
         if (image_array[imageNumber] != "0.jpg") {
             document.getElementById('image_shower').src = `./images/${image_array[imageNumber]}`
-            console.log(image_array[imageNumber]);
         }
     }, intervalSpeed);
+}
+
+function preventMultipleClick() {
+    // prevent multiple click
+    if (lastClick >= (Date.now() - delay))
+        return;
+    lastClick = Date.now();
 }
